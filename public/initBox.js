1faba14e48551a8be1606545b058c82df5f0c84b
Joshua Smith <subjanus@gmail.com>
2026-01-01 00:09:35 -0500

    Gotdot version, testing chatgpt5.2

--- public/initBox.js
+++ public/initBox.js
@@ -7,7 +7,7 @@ export function initBox(scene, hex = "#00A3FF") {
   mat.diffuseColor = BABYLON.Color3.FromHexString(hex);
   box.material = mat;
 
-  // 🔽 DROP THE CUBE WELL BELOW CAMERA LEVEL
+  // 🔽 DROP THE CUBE WELL BELOW CAMERA LEVEL 
   box.position.x = 0;
   box.position.y = -10;   // <<<<< THIS IS THE KEY LINE
   box.position.z = 0;
